1014646cb344f264b7d68d551af4b9059943b666
Matt Linaberry <matt.linaberry@we-ics.com>
2015-03-14 11:52:17 -0400

    The book store!

--- Bookstore/main.cpp
+++ Bookstore/main.cpp
@@ -7,9 +7,26 @@
 //
 
 #include <iostream>
+#include "Sales_item.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    Sales_item total;
+    if (std::cin >> total ) {
+        Sales_item trans;
+        while (std::cin >> trans) {
+            if (total.isbn() == trans.isbn()) {
+                total += trans;
+            }
+            else {
+                std::cout << total << std::endl;
+                total = trans;
+            }
+        }
+        std::cout << total << std::endl;
+    }
+    else {
+        std::cerr << "No data?!?!?" << std::endl;
+        return -1;
+    }
     return 0;
 }
